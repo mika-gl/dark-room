8a9f4df85918facd7e0fc07cd0d1147a1a58fa71
Mikail Gonullu <rallynorrly@gmail.com>
2024-08-11 13:14:25 -0400

    exp branch initial

--- functions.js
+++ functions.js
@@ -7,39 +7,68 @@ function parseToGridArea(array) {       //para convertir de vuelta, despues de s
 //FUNCIONES DE EVENTO
 function moveElement(element, padding, event) {
 	//gridArea Actual de character, que seria donde esta 'posicionado'
-        let oldElementAreaArray = getComputedStyle(element).gridArea.match(regex);
+	let oldElementAreaArray = getComputedStyle(element).gridArea.match(regex);
+	switch (event.key) {
+		case 'w':
+			oldElementAreaArray[0]-=2;         //los strings se coercionan a ints      
+			oldElementAreaArray[2]-=2;                                                
+			element.style.gridArea = parseToGridArea(oldElementAreaArray);
+			break;
+	case 'a':
+			oldElementAreaArray[1]-=2;
+			oldElementAreaArray[3]-=2;
+			element.style.gridArea = parseToGridArea(oldElementAreaArray);
+			break;
+	case 's':
+			oldElementAreaArray[0]-=(-2);      //para que sigan coercionando a ints. Si sumo se concatenaran   
+			oldElementAreaArray[2]-=(-2);
+			element.style.gridArea = parseToGridArea(oldElementAreaArray);
+			break;
+	case 'd':
+			oldElementAreaArray[1]-=(-2);
+			oldElementAreaArray[3]-=(-2);
+			element.style.gridArea = parseToGridArea(oldElementAreaArray);
+			break;
+	}
+}
 
-        switch (event.key) {
-                case 'w':
-                        if (oldElementAreaArray[0]> 1+padding) {	//oldCharArray. Quizas hacer nuevo array: newCharAreaArray 
-                                oldElementAreaArray[0]-=2;         //los strings se coercionan a ints      
-                                oldElementAreaArray[2]-=2;                                                
-                                element.style.gridArea = parseToGridArea(oldElementAreaArray);
-                        }
-                        break;
-                case 'a':
-                        if (oldElementAreaArray[1] > 1+padding) {
-                                oldElementAreaArray[1]-=2;
-                                oldElementAreaArray[3]-=2;
-                                element.style.gridArea = parseToGridArea(oldElementAreaArray);
-                        }
-                        break;
-                case 's':
-                        if (oldElementAreaArray[2] < constructedRoomSize-padding) {
-                                oldElementAreaArray[0]-=(-2);      //para que sigan coercionando a ints. Si sumo se concatenaran   
-                                oldElementAreaArray[2]-=(-2);
-                                element.style.gridArea = parseToGridArea(oldElementAreaArray);
-                        }
-                        break;
-                case 'd':
-                        if (oldElementAreaArray[3] < constructedRoomSize-padding) {
-                                oldElementAreaArray[1]-=(-2);
-                                oldElementAreaArray[3]-=(-2);
-                                element.style.gridArea = parseToGridArea(oldElementAreaArray);
-                        }
-                        break;
-                //final de switch
-        }
+function checkObjectCollision(key) {
+	let objectCoordinates = {};
+	roomObjects.forEach((obj) => {
+		objectCoordinates[getCoordinates(obj).x] = getCoordinates(obj).y; //guardar la informacion de ambas coordenadas en key y value
+	});
+	let character_x = getCoordinates(character).x;
+	let character_y = getCoordinates(character).y;
+
+	switch (key) {
+		case "w":
+			return !(character_y - 1 === objectCoordinates[character_x]);
+			//break;
+		case "s":
+			return !(character_y + 1 === objectCoordinates[character_x]);
+			//break;
+		case "a":
+			return !(objectCoordinates[character_x - 1] === character_y);
+			//break;
+		case "d":
+			return !(objectCoordinates[character_x + 1] === character_y);
+			//break;
+	}
+	return true;
+}
+function checkWorldCollision(key) {
+	let character_x = getCoordinates(character).x;
+	let character_y = getCoordinates(character).y;
+	switch (key) {
+		case "w":
+			return character_y > 1;
+		case "s":
+			return character_y < 17;
+		case "a":
+			return character_x > 1;
+		case "d":
+			return character_x < 17;
+	}
 }
 var footstepsCounter = 1;
 function playFootsteps() {
@@ -61,19 +90,36 @@ function playFootsteps() {
 }
 
 function getCoordinates(element) {
-        let elementAreaArray = getComputedStyle(element).gridArea.match(regex);
+    let elementAreaArray = getComputedStyle(element).gridArea.match(regex);
 	let coordinates = {
 		x: (elementAreaArray[1]-charPadding+1)/2, //transformacion a plano, pues gridArea aumenta de a 2	
 		y: (elementAreaArray[0]-charPadding+1)/2  
 	}
 	return coordinates;
 }
+function getShadowCoordinates(element) {
+	let elementAreaArray = getComputedStyle(element).gridArea.match(regex);
+	let coordinates = {
+		x: (parseInt(elementAreaArray[1])+1)/2, //transformacion a plano, pues gridArea aumenta de a 2	
+		y: (parseInt(elementAreaArray[0])+1)/2  
+	}
+	return coordinates;
+}
+function translateShadowCoordinates(element) {
+	let coordinates = {
+		x: getCoordinates(character).x - (4 + 1 - getShadowCoordinates(element).x),
+		y: getCoordinates(character).y - (4 + 1 - getShadowCoordinates(element).y)
+	}
+	return coordinates;
+}
+
 function coordinatesToGridArea(coordinates, elementPadding, elementSize) {
 	let rowStart = (coordinates.y*2)+elementPadding-1;
 	let colStart = (coordinates.x*2)+elementPadding-1;
 	let gridArea = `${rowStart}/${colStart}/${rowStart+elementSize}/${colStart+elementSize}`;
 	return gridArea;
 }
+
 function setElementXY(element, coordinates, elementPadding, elementSize) {	//quizas usar esta funciona para mover char con menos codigo
 	element.style.gridArea = coordinatesToGridArea(coordinates, elementPadding, elementSize);
 }
@@ -94,5 +140,10 @@ function createShadowBits() {
 	}
 }
 
-
+function distributeShadowHiders() {
+	shadowHiders[0].style.gridArea = `1/1/${constructedRoomSize+1}/${charPadding+1}`;
+	shadowHiders[1].style.gridArea = `1/${constructedRoomSize-charPadding+1}/${constructedRoomSize+1}/${constructedRoomSize+1}`;
+	shadowHiders[2].style.gridArea = `1/${charPadding+1}/${charPadding+1}/${constructedRoomSize-charPadding+1}`;
+	shadowHiders[3].style.gridArea = `${constructedRoomSize-charPadding+1}/${charPadding+1}/${constructedRoomSize+1}/${constructedRoomSize-charPadding+1}`;
+}
 
--- init.js
+++ init.js
@@ -1,12 +1,13 @@
-//buscar pointers de HTML DOM
+//buscar referencias de HTML DOM
 var character = document.getElementById('character');
 var shadow = document.getElementById('shadow');
 var constructedRoom = document.querySelector('.constructed-room'); //el cuarto GRID PADRE que limita la sombras
 var characterRoom = document.querySelector('.character-room'); //el cuarto que limita el personaje
 var shadowHiders = document.querySelectorAll('.hides-shadow');
+var doors = document.querySelectorAll('.door');
+
 //objetos
 var roomObjects = document.querySelectorAll('.object');
-var shadowBits = document.querySelectorAll('.shadow-bit');
 var regex = /\d+/g;	//regex para buscar los digitos de ComputedStyle de GridArea
 
 
@@ -28,39 +29,72 @@ setElementXY(character, {x:10,y:10}, charPadding, charSize);
 setElementXY(shadow, {x:10,y:10}, 0, shadowSize);
 characterRoom.style.gridArea = `${charPadding+1}/${charPadding+1}/${constructedRoomSize-charPadding+1}/${constructedRoomSize-charPadding+1}`;
 
-//shadow bits
-createShadowBits();
 
-//shadow hiders fuera del mapa
-shadowHiders[0].style.gridArea = `1/1/${constructedRoomSize+1}/${charPadding+1}`;
-shadowHiders[1].style.gridArea = `1/${constructedRoomSize-charPadding+1}/${constructedRoomSize+1}/${constructedRoomSize+1}`;
-shadowHiders[2].style.gridArea = `1/${charPadding+1}/${charPadding+1}/${constructedRoomSize-charPadding+1}`;
-shadowHiders[3].style.gridArea = `${constructedRoomSize-charPadding+1}/${charPadding+1}/${constructedRoomSize+1}/${constructedRoomSize-charPadding+1}`;
+distributeShadowHiders();	//necesita valores declarados arriba
+createShadowBits();			//necesita valores declarados arriba
+var shadowBits = document.querySelectorAll('.shadow-bit');
+
+
+//crear en grid 'doors'
+doors.forEach((element) => {
+	let position = {};
+	switch (element.id) {
+		case "door-top":
+			position = {x: 10, y: 1}
+			setElementXY(element, position, 6,2);
+			break;
+		case "door-bottom":
+			position = {x: 10, y: 19}
+			setElementXY(element, position, 6,2);
+			break;
+		case "door-left":
+			position = {x: 1, y: 10}
+			setElementXY(element, position, 6,2);
+			break;
+		case "door-right":
+			position = {x: 19, y: 10}
+			setElementXY(element, position, 6,2);
+			break;
+	}
+});
+
+
 
 
 //mover personaje
 var justMoved = false;
 var movementKeys = ['w','a','s','d'];
+
 document.addEventListener('keydown', function keyDownHandler(event) {
 	if (!movementKeys.includes(event.key)) { return; }
-	if (!justMoved) {
+
+	if (!justMoved && checkObjectCollision(event.key) && checkWorldCollision(event.key)) {
 		justMoved = true;
 		moveElement(character, charPadding, event);	
 		moveElement(shadow, 0, event);
 		playFootsteps();
 	
 		setTimeout(() => { justMoved = false; }, 300); //duracion de audios 250ms
-		//console.log(getCoordinates(character));
+		console.log(`character moved to: ${getCoordinates(character).x},${getCoordinates(character).y}`);
 
 	} else {
 		console.log("can't move yet");
 	}
-});
-
-//deteccion de sombra en cada shadow bit 
-shadowBits.forEach((bit) => {
-	bit.addEventListener('keydown', function shadowBitHandler() {
-		
+	
+	
+	//deteccion de sombra en cada shadow bit 
+	shadowBits.forEach((bit) => {
+		roomObjects.forEach((roomObject) => {
+			if (translateShadowCoordinates(bit).x == getCoordinates(roomObject).x && translateShadowCoordinates(bit).y == getCoordinates(roomObject).y) {
+				roomObject.style.zIndex = `${parseInt(window.getComputedStyle(characterRoom).zIndex) + 1}`;
+				bit.style.zIndex = `${parseInt(window.getComputedStyle(roomObject).zIndex) - 1}`;
+				console.log(bit);
+			} else {
+				roomObject.style.zIndex = `${parseInt(window.getComputedStyle(characterRoom).zIndex) - 1}`;
+				bit.style.zIndex = `${parseInt(window.getComputedStyle(roomObject).zIndex) + 1}`;
+			}
+		});
 	});
+	console.log(`an object is at: ${getCoordinates(roomObjects[0]).x},${getCoordinates(roomObjects[0]).y}`);
+	
 });
-console.log(getCoordinates(roomObjects[0]));
